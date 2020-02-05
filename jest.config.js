bbe7a34c70680324cf357285e9fef1a497dfb8ae
Michael Underwood <mike.underwood@gmail.com>
2020-02-04 21:53:51 -0700

    Set up internal path resolution for @mmsi

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig");
+
 module.exports = {
   "roots": [
     "<rootDir>/tests"
   ],
   "transform": {
     "^.+\\.ts$": "ts-jest"
-  }
-}
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
+};
